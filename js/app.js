3a00bc03074fcb135852ac86c2d66176d7f057f4
Sean Kagawa <seankagawa@gmail.com>
2018-02-28 14:50:19 -1000

    compl ex2

--- js/app.js
+++ js/app.js
@@ -6,8 +6,16 @@ The function will add the following content inside the paragraph with the id of 
 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
+var moreContentIgnition = false;
+
 function moreContent(){
-	document.getElementById('more').innerHTML = bacon;
+	if(moreContentIgnition === false){
+		moreContentIgnition = true;
+		document.getElementById('more').innerHTML = bacon;
+	}else{
+		moreContentIgnition = false;
+		document.getElementById('more').innerHTML = '';
+	}
 }
 
 //2. HTTP
@@ -15,7 +23,20 @@ function moreContent(){
 
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
 
-document.getElementById('noMore').addEventListener('click', lessContent);
+var lessContentContainer = '';
+
+var lessContentIgnition = false;
+
+function lessContent(){
+	if(lessContentIgnition === false){
+		lessContentIgnition = true;
+		lessContentContainer = document.getElementById('less').innerHTML;
+		document.getElementById('less').innerHTML = "";
+	}else{
+		lessContentIgnition = false;
+		document.getElementById('less').innerHTML = lessContentContainer;
+	}
+}
 
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
